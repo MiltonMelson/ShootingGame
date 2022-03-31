a8875cabd718ae3e33964cb0acf8e7a41c30b5f2
Milton Melson <77636982+MiltonMelson@users.noreply.github.com>
2022-03-31 13:12:33 -0700

    Add files via upload

---
+++ ShootingGame/DoctorWhoShootingGame/ShootingGame/README.md
@@ -0,0 +1,12 @@
+## Shooting Game
+
+## Description
+- Shows how to use canvas to draw and move shapes.
+- Shows how to detect collisions between these objects/shapes.
+- Shows how to use eventListeners to detect keyboard keys for movement on the x and y axis. 
+- Shows the uses of id, classes, and keyframes in CSS/HTMl.
+
+
+## Resources
+https://www.w3schools.com/html/html5_canvas.asp
+https://www.w3schools.com/tags/ref_canvas.asp
---
+++ ShootingGame/Enemy.js
@@ -0,0 +1,39 @@
+export default class Enemy {
+    constructor(x, y, color, health) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.health = health;
+        this.width = 50;
+        this.height = 50;
+    }
+
+    draw(ctx) {
+        // Draw enemy box
+        ctx.fillStyle = this.color;
+        ctx.strokeStyle = this.shields();
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+
+        // Draw Health
+        ctx.fillStyle = this.shields();
+        ctx.font = "25px Arial";
+        ctx.fillText(this.health, this.x + this.width / 4, this.y + this.height / 1.5);
+    }
+
+    shields() {
+        if (this.health >= 20) {
+            return "white";
+        }
+        else if (this.health >= 8) {
+            return "orange";
+        }
+        else {
+            return "red";
+        }
+    }
+
+    takeDamage(damage) {
+        this.health -= damage;
+    }
+}
